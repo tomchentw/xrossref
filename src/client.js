1429a48146992fe82b05e043a28a5425a8a757ee
Tom Chen <developer@tomchentw.com>
2015-06-09 15:43:59 +0800

    chore(src): move ReactRoot to components

--- src/client.js
+++ src/client.js
@@ -1,7 +1,7 @@
 import {default as React} from "react";
 import {default as injectTapEventPlugin} from "react-tap-event-plugin";
 
-import {default as ReactRoot} from "./ReactRoot";
+import {default as ReactRoot} from "./components/ReactRoot";
 
 //Needed for onTouchTap
 //Can go away when react 1.0 release
--- src/server.js
+++ src/server.js
@@ -1,5 +1,5 @@
 import {default as React} from "react";
 
-import {default as ReactRoot} from "./ReactRoot";
+import {default as ReactRoot} from "./components/ReactRoot";
 
 export default ReactRoot;
