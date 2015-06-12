92e6f93fd85c4fe40e64fa77cf29e94535cff246
Tom Chen <developer@tomchentw.com>
2015-06-13 00:31:35 +0800

    fix(Kimono): once at a time

--- src/api/Kimono.js
+++ src/api/Kimono.js
@@ -4,10 +4,16 @@ function asJson (res) {
 
 const API_KEY = `b4d0a97c118372aef4d4f63afb686ca6`;
 
+let last = Promise.resolve();
+
+function oneRequestAtATime (...argsForFetch) {
+  return last = last.then(_ => fetch(...argsForFetch));
+}
+
 export function issuesCountsInfo (ownerRepoStr) {
   const [owner, repo] = ownerRepoStr.split("/");
 
-  return fetch(
+  return oneRequestAtATime(
     `https://www.kimonolabs.com/api/ondemand/9a3om980?apikey=${ API_KEY
     }&kimmodify=1&kimpath1=${ owner }&kimpath2=${ repo }`
   )
