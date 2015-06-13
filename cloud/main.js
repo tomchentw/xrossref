f416449b330b5a0a20ddba47287602fbdad83ba2
Tom Chen <developer@tomchentw.com>
2015-06-13 20:51:31 +0800

    feat(Cloud): use webpack to compile parse cloud code

--- cloud/main.js
+++
@@ -1,6 +0,0 @@
-require("cloud/GitHubRepoInfo");
-// Use Parse.Cloud.define to define as many cloud functions as you want.
-// For example:
-Parse.Cloud.define("hello", function(request, response) {
-  response.success("Hello world!");
-});
---
+++ src/parse.js
@@ -0,0 +1,1 @@
+import * as GitHub from "./cloud/GitHub";
