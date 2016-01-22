d917ca477d508bdeaa15d65cdfa3284ca5914241
Tom Chen <developer@tomchentw.com>
2016-01-22 19:35:08 +0800

    fix(fetchPage): it's options

--- src/cloud/fetchPage.js
+++ src/cloud/fetchPage.js
@@ -4,7 +4,7 @@
 const UA = `Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_3) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/43.0.2357.124 Safari/537.36`;
 /* eslint-enable max-len */
 
-export default function fetchPage({ options }) {
+export default function fetchPage(options) {
   return Parse.Cloud.httpRequest({
     ...options,
     headers: {
